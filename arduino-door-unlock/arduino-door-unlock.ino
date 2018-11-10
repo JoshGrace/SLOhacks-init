4852ab021441d0ab76a3a3c6a5cae204530e782c
etnom <montgomery.okdaily@gmail.com>
2018-11-10 09:19:47 -0800

    servo set up stuff

--- arduino-door-unlock/arduino-door-unlock.ino
+++ arduino-door-unlock/arduino-door-unlock.ino
@@ -1,7 +1,13 @@
 #include <Wire.h>
-#include <Serial.h>
+#include <SoftwareSerial.h>
+#include <Servo.h>
 
 #define DOOR_UNLOCK_PIN 1
+#define DOOR_SERVO_PIN 3
+
+Servo doorServo;
+
+int servoAngle = 0;
 
 void setup () {
 	pinMode(DOOR_UNLOCK_PIN, OUTPUT);
@@ -11,14 +17,24 @@ void setup () {
 	//join i2c bus with ardress 8
 	Wire.begin(8);
 	Wire.onReceive(receiveHandler);
+
+	//Servo setup
+	doorServo.attach(DOOR_SERVO_PIN);
 }
 
 void loop() {
-
+	
 }
 
-void receiveHandler() {
+//event is int received from RPI
+//0 = lock door
+//1 = unlock door
+void receiveHandler(int a) {
 	int event = Wire.read();    // receive byte as an integer
 	Serial.println(event);
+}
+
+void unlockDoor() {
+
 }
 
