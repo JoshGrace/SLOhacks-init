689b3424e7e9cd0f1686f6dea097e9f7a4e64e66
etnom <montgomery.okdaily@gmail.com>
2018-11-10 08:48:27 -0800

    functional template for I2C arduino as slave

--- arduino-door-unlock/arduino-door-unlock.ino
+++ arduino-door-unlock/arduino-door-unlock.ino
@@ -1,9 +1,24 @@
+#include <Wire.h>
+#include <Serial.h>
+
 #define DOOR_UNLOCK_PIN 1
 
 void setup () {
 	pinMode(DOOR_UNLOCK_PIN, OUTPUT);
+
+	Serial.begin(9600);
+
+	//join i2c bus with ardress 8
+	Wire.begin(8);
+	Wire.onReceive(receiveHandler);
 }
 
 void loop() {
-	
+
 }
+
+void receiveHandler() {
+	int event = Wire.read();    // receive byte as an integer
+	Serial.println(event);
+}
+
