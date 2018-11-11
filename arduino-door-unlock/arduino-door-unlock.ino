5ce027b4479da38df9ff828e9f73203422931242
etnom <montgomery.okdaily@gmail.com>
2018-11-10 18:06:00 -0800

    motor to servo

--- arduino-door-unlock/arduino-door-unlock.ino
+++ arduino-door-unlock/arduino-door-unlock.ino
@@ -1,12 +1,10 @@
 #include <SoftwareSerial.h>
-#include <CheapStepper.h>
+#include <Servo.h>
 
-#define DOOR_UNLOCK_PIN 2
+#define LOCK_UNLOCK_PIN 6
 
-#define IN1 8
-#define IN2 9
-#define IN3 10
-#define IN4 11
+#define LOCKED_SERVO_ANGLE 0
+#define UNLOCKED_SERVO_ANGLE 180
 
 #define PUSHER_PIN 3
 #define FLYWHEELS_PIN 5
@@ -14,16 +12,19 @@
 #define FLYWHEEL_ACCEL_TIME 100
 #define ONE_DART_TIME 100
 
-CheapStepper doorStepper (IN1, IN2, IN3, IN4);
+Servo lockServo;
 
 //0 = locked
 //1 = unlocked
 bool lockState = 0;
 
-void setup () {
-	pinMode(DOOR_UNLOCK_PIN, OUTPUT);
+int lastFireChar = 0;
 
+void setup () {
 	Serial.begin(9600);
+
+	pinMode(LOCK_UNLOCK_PIN, OUTPUT);
+	lockServo.attach(LOCK_UNLOCK_PIN);
 
 	pinMode(PUSHER_PIN, OUTPUT);
 	pinMode(FLYWHEELS_PIN, OUTPUT);
@@ -33,33 +34,34 @@ void setup () {
 }
 
 void loop() {
+	// lockServo.write(0);
+	// delay(2000);
+	// lockServo.write(180);
+	// delay(2000);
 	readSerial();
 }
 
 void readSerial() {
 	//read Serial for commands
 	if(Serial.available() > 0) {
-       handleSerialReading(Serial.read());
+		//convert ascii val of Serial.read() to char
+       handleSerialReading(Serial.read() - 48);
     }
 }
 
 //serialReading in ASCII
 void handleSerialReading(int serialReading) {
-	int charSerialReading = serialReading - 48;	//convert ascii to char
-
-	Serial.println("Got " + (String)charSerialReading);
-
+	Serial.println((String)serialReading);
 
 	//in = 0 means lock
 	//in - 1 means lock
-	if (charSerialReading <= 1) {
-		moveLock(charSerialReading);
+	if (serialReading <= 1) {
+		moveLock(serialReading);
 		return;
 	}
-
-	//fire should be a num greater than 10
-	//11 = 1 dart shoot
-	fire(charSerialReading - 10);
+		
+	//fire if anything from serial isn't 1 or 0
+	fire();
 }
 
 //function to unlock or lock door
@@ -68,28 +70,37 @@ void handleSerialReading(int serialReading) {
 //clockwise = lock
 //CCW = unlock
 void moveLock(bool toLock) {
-	if (toLock != lockState) {
-		Serial.println("New lock state " + (String)toLock);
-		for (int i = 0; i < 1024; i++) {
-			doorStepper.step(toLock);
-			Serial.println((String)doorStepper.getStepsLeft());
+	if (toLock != lockState) {	
+		//lock	
+		if (toLock) {
+			Serial.println("Locking");
+			lockServo.write(LOCKED_SERVO_ANGLE);
+			lockState = toLock;
+			delay(100);
+			return;
 		}
+
+		//unlock
+		Serial.println("Unlcoking");
+		lockServo.write(UNLOCKED_SERVO_ANGLE);
+		delay(5000);
+		lockServo.write(LOCKED_SERVO_ANGLE);
 
 		lockState = toLock;
 	}
 	
 }
 
-void fire(int numOfDarts) {
-	Serial.println("Firing " + (String)numOfDarts + " darts!");
+void fire() {
+	Serial.println("firig");
 
 	//accel flywheel motors
 	digitalWrite(FLYWHEELS_PIN, HIGH);
 	delay(FLYWHEEL_ACCEL_TIME);
 
-	//power pusher motor for certain time depending on how many darts need to be fired
+	//power pusher motor for certain time to shoot one dart
 	digitalWrite(PUSHER_PIN, HIGH);
-	delay(ONE_DART_TIME * numOfDarts);
+	delay(ONE_DART_TIME);
 
 	digitalWrite(PUSHER_PIN, LOW);
 	digitalWrite(FLYWHEELS_PIN, LOW);	
